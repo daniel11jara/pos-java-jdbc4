0ddb261a8907abb3df445f36db0d62c2a15aa5c6
daniel jara <daniel11jara@gmail.com>
2023-06-08 17:37:57 -0300

    criação modulo 21

---
+++ pos-java-jdbc4/src/main/java/pos_java_jdbc4/pos_java_jdbc4/App.java
@@ -0,0 +1,13 @@
+package pos_java_jdbc4.pos_java_jdbc4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
